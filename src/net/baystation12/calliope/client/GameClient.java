55a45875653f7f99bb50850bbda2a4fc345dfed0
DJSnapshot <trevornaze@gmail.com>
2014-01-28 13:04:01 -0800

    fixed because SkyMarshal is a butt.

--- src/net/baystation12/calliope/client/GameClient.java
+++ src/net/baystation12/calliope/client/GameClient.java
@@ -20,7 +20,7 @@ import org.lwjgl.Sys;
 public class GameClient extends Listener {
 	public static GameClient client;
 	public static Client connection;
-        public static String cversion = "0.1-a1";
+	public static String cversion = "0.1-a1";
 	
 	private static Map<Integer, Integer> entity_by_id;
 	private static boolean running = false;
@@ -29,19 +29,19 @@ public class GameClient extends Listener {
 	private static int present_tick = 0;
 	private static long last_server_tick = 0;
 	
-        private static int displaywidth = 800;
-        private static int displayheight = 600;
-        
-        private static long FPS = 0;
-        private static long lastFPS = 0;
-        
+	private static int displaywidth = 800;
+	private static int displayheight = 600;
+	
+	private static long FPS = 0;
+	private static long lastFPS = 0;
+	
 	public static void main(String[] args) throws LWJGLException, InterruptedException
 	{
 		client = new GameClient();
-                lastFPS = getTime();
+		lastFPS = getTime();
 		Display.setDisplayMode(new DisplayMode(displaywidth, displayheight));
 		Display.create();
-                
+
 
 		GameClient.running = true;
 		while(GameClient.running)
@@ -51,9 +51,9 @@ public class GameClient extends Listener {
 			{
 				GameClient.running = false;
 			}
-                        
+
 			Display.update();
-                        updateFPS();
+			updateFPS();
 			Thread.sleep(1000/60);
 		}
 		Display.destroy();
@@ -64,24 +64,23 @@ public class GameClient extends Listener {
 		this("127.0.0.1", 9000, 8000);
 	}
 	
-        public static void updateFPS()
-        {
-            if (getTime() - lastFPS > 1000 )
-            {
-                String displaytitle = "Project Calliope v." + cversion + " FPS: " + FPS;
-                Display.setTitle(displaytitle);
-                FPS = 0;
-                lastFPS += 1000;
-            }
-            FPS ++;
-        }
-        
-        public static long getTime()
-        {
-            return (Sys.getTime() * 1000) / Sys.getTimerResolution();
-            
-        }
-        
+	public static void updateFPS()
+	{
+	    if (getTime() - lastFPS > 1000 )
+	    {
+		String displaytitle = "Project Calliope v." + cversion + " FPS: " + FPS;
+		Display.setTitle(displaytitle);
+		FPS = 0;
+		lastFPS += 1000;
+	    }
+	    FPS ++;
+	}
+
+	public static long getTime()
+	{
+	return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+
 	private GameClient(String server_address, int tcp_address, int udp_address)
 	{
 		connection = new Client();
