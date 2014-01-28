8e52571a95f5b9b47a221e9ec51c6ad802fb1d1f
DJSnapshot <trevornaze@gmail.com>
2014-01-28 12:45:57 -0800

    FPS implementation and versioning for the client. All info is passed into the title for now.
    Also, window size passed into variables for future implementation of client configs for custom size and menu positions.

--- src/net/baystation12/calliope/client/GameClient.java
+++ src/net/baystation12/calliope/client/GameClient.java
@@ -14,11 +14,13 @@ import org.lwjgl.opengl.DisplayMode;
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
+import org.lwjgl.Sys;
 
 
 public class GameClient extends Listener {
 	public static GameClient client;
 	public static Client connection;
+        public static String cversion = "0.1-a1";
 	
 	private static Map<Integer, Integer> entity_by_id;
 	private static boolean running = false;
@@ -27,12 +29,19 @@ public class GameClient extends Listener {
 	private static int present_tick = 0;
 	private static long last_server_tick = 0;
 	
+        private static int displaywidth = 800;
+        private static int displayheight = 600;
+        
+        private static long FPS = 0;
+        private static long lastFPS = 0;
+        
 	public static void main(String[] args) throws LWJGLException, InterruptedException
 	{
 		client = new GameClient();
-
-		Display.setDisplayMode(new DisplayMode(800, 600));
+                lastFPS = getTime();
+		Display.setDisplayMode(new DisplayMode(displaywidth, displayheight));
 		Display.create();
+                
 
 		GameClient.running = true;
 		while(GameClient.running)
@@ -42,8 +51,9 @@ public class GameClient extends Listener {
 			{
 				GameClient.running = false;
 			}
-
+                        
 			Display.update();
+                        updateFPS();
 			Thread.sleep(1000/60);
 		}
 		Display.destroy();
@@ -54,6 +64,24 @@ public class GameClient extends Listener {
 		this("127.0.0.1", 9000, 8000);
 	}
 	
+        public static void updateFPS()
+        {
+            if (getTime() - lastFPS > 1000 )
+            {
+                String displaytitle = "Project Calliope v." + cversion + " FPS: " + FPS;
+                Display.setTitle(displaytitle);
+                FPS = 0;
+                lastFPS += 1000;
+            }
+            FPS ++;
+        }
+        
+        public static long getTime()
+        {
+            return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+            
+        }
+        
 	private GameClient(String server_address, int tcp_address, int udp_address)
 	{
 		connection = new Client();
